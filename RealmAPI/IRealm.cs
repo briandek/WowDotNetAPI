aa8d3b8e60ad94cbcdec562b806fb6dbea42eeb4
Briam Ramos <briandek@gmail.com>
2011-04-26 08:25:49 -0400

    adding IRealm

---
+++ RealmAPI/IRealm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RealmAPI
+{
+    public interface IRealm
+    {
+        string name { get; set; }
+        string slug { get; set; }
+        string type { get; set; }
+        bool status { get; set; }
+        bool queue { get; set; }
+        string population { get; set; }
+    }
+}
