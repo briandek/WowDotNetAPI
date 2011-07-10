84dfab2a874ba3ded00936aa668d261ce13efad4
Briam Ramos <briandek@gmail.com>
2011-07-10 03:56:52 -0400

    simple GuildExplorer tests

---
+++ Explorers.Test/GuildExplorerTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WowDotNetAPI.Explorers.Interfaces;
+using WowDotNetAPI.Explorers.Utilities;
+using System.Net;
+using System.Web.Script.Serialization;
+using WowDotNetAPI.Explorers.Models;
+
+namespace WowDotNetAPI.Explorers.Test
+{
+    [TestClass]
+    public class GuildExplorerTest
+    {
+        Guild immortalityGuild;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            immortalityGuild = TestUtil.gE.Guild;
+        }
+
+        [TestMethod]
+        public void Get_Simple_Guild_Immortality_From_Skullcrusher()
+        {
+            Assert.IsTrue(immortalityGuild.name.Equals("immortality", StringComparison.InvariantCultureIgnoreCase));
+            Assert.IsTrue(immortalityGuild.realm.Equals("skullcrusher", StringComparison.InvariantCultureIgnoreCase));
+            Assert.IsTrue(immortalityGuild.members.Any());
+        }
+
+        [TestMethod]
+        public void Get_Valid_Member_From_Immortality_Guild()
+        {
+            Member briandek = immortalityGuild.members.Where(m => m.character.name.Equals("briandek", StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
+
+            Assert.IsTrue(briandek.character.name.Equals("briandek", StringComparison.InvariantCultureIgnoreCase));
+            Assert.IsTrue(briandek.character.@class == 1);
+            Assert.IsTrue(briandek.character.gender == 0);
+            //Assert.IsTrue(briandek.character.achievementPoints == 6895); achievements via guild api is broken ._.
+
+        }
+
+        [TestMethod]
+        public void Get_Valid_Member_From_Another_Guild()
+        {
+            TestUtil.gE = new GuildExplorer("Laughing Skull", "Deus Vox");
+            Guild dvGuild = TestUtil.gE.Guild;
+
+            Assert.IsTrue(dvGuild.name.Equals("deus vox", StringComparison.InvariantCultureIgnoreCase));
+            Assert.IsTrue(dvGuild.realm.Equals("laughing skull", StringComparison.InvariantCultureIgnoreCase));
+            Assert.IsTrue(dvGuild.members.Any());
+
+
+            Member ohnoes = dvGuild.members.Where(m => m.character.name.Equals("ohnoes", StringComparison.InvariantCultureIgnoreCase)).FirstOrDefault();
+            Assert.IsTrue(ohnoes.character.name.Equals("ohnoes", StringComparison.InvariantCultureIgnoreCase));
+            Assert.IsTrue(ohnoes.character.@class == 4);
+            Assert.IsTrue(ohnoes.character.gender == 1);
+        }
+    }
+}
---
+++ Explorers.Test/TestUtil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WowDotNetAPI.Explorers.Interfaces;
+
+namespace WowDotNetAPI.Explorers.Test
+{
+    [TestClass]
+    public class TestUtil
+    {
+        public static IRealmExplorer rE;
+        public static IGuildExplorer gE;
+
+        [AssemblyInitialize]
+        public static void GlobalStartup(TestContext testContext)
+        {
+            rE = new RealmExplorer();
+            gE = new GuildExplorer("skullcrusher", "immortality");
+
+        }
+    }
+}
