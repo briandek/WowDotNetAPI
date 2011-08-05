5cde866a1af5de7a9b7f2a546ba2b7f6dd8b3220
Mark de Jong <mail@markdejong.org>
2011-08-05 20:36:17 +0200

    Added another test (Embersilk Cloth linq check)

--- Explorers.Test/AuctionTest.cs
+++ Explorers.Test/AuctionTest.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WowDotNetAPI.Models;
 using WowDotNetAPI.Test;
 
 namespace WowDotNetAPI.Explorers.Test
@@ -13,7 +14,10 @@ namespace WowDotNetAPI.Explorers.Test
         [TestMethod]
         public void testAuctionsCount()
         {
-            Assert.IsTrue(TestUtility.WowExplorer.GetAuctions("Skullcrusher").Horde.Auctions.Count() > 0);
+            Auctions actions = TestUtility.WowExplorer.GetAuctions("Skullcrusher");
+
+            Assert.IsTrue(actions.Horde.Auctions.Count() > 0);
+            Assert.IsTrue((from n in actions.Horde.Auctions where n.ItemId == 53010 select n).Count() > 0);
 
         }
     }
