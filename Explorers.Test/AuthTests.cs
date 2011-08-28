93625b7a04da7a7711a4d2ef63b5cf88f99b7311
Briam Ramos <briandek@gmail.com>
2011-08-28 12:24:27 -0400

    using httpwebrequest

---
+++ Explorers.Test/AuthTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WowDotNetAPI.Models;
+
+namespace WowDotNetAPI.Explorers.Test
+{
+    [TestClass]
+    public class AuthTests
+    {
+        [TestMethod]
+        public void FetchData_When_Authenticated()
+        {
+            WowExplorer authExplorer = new WowExplorer(Region.US, Locale.en_US, "", "");
+
+            IEnumerable<Realm> realms = authExplorer.GetRealms();
+
+            Realm realm = realms.GetRealm("skullcrusher");
+            Assert.IsNotNull(realm);
+            Assert.IsTrue(realm.Name == "Skullcrusher");
+            Assert.IsTrue(realm.Type == RealmType.PVP);
+            Assert.IsTrue(realm.Slug == "skullcrusher");
+        }
+    }
+}
