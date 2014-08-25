583e4e90a9e98055ef0effb9e1a211a732af7c4f
jsassner <johan@sassner.com>
2014-08-25 14:50:45 +0200

    Added <stats> to Items. Added <upgrade> to ItemToolTipParameters

---
+++ Explorers/Models/ItemStat.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace WowDotNetAPI.Models {
+	[DataContract]
+	public class ItemStat {
+		[DataMember(Name = "stat")]
+		public int Stat { get; set; }
+
+		[DataMember(Name = "amount")]
+		public int Amount { get; set; }
+
+		[DataMember(Name = "reforgedAmount")]
+		public int ReforgedAmount { get; set; }
+
+		[DataMember(Name = "reforged")]
+		public bool Reforged { get; set; }
+
+	}
+}
---
+++ Explorers/Models/ItemUpgrade.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace WowDotNetAPI.Models {
+	[DataContract]
+	public class ItemUpgrade {
+		[DataMember(Name = "current")]
+		public int Current { get; set; }
+
+		[DataMember(Name = "total")]
+		public int Total { get; set; }
+
+		[DataMember(Name = "itemLevelIncrement")]
+		public int ItemLevelIncrement { get; set; }
+
+	}
+}
