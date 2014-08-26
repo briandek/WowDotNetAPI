88df1fc1a35fa4831f6ec58b10c456ee2b5f8128
Briam Ramos <briandek@gmail.com>
2014-08-26 00:57:04 -0700

    Z

--- Explorers.Test/CharacterTests.cs
+++ Explorers.Test/CharacterTests.cs
@@ -11,7 +11,7 @@ namespace WowDotNetAPI.Test
     public class CharacterTests
     {
         private static WowExplorer explorer;
-        private static string APIKey = "kpvwrqw2abwjh8crhsvsx7p4cbhxpmtk";
+        private static string APIKey = "";
 
         [ClassInitialize]
         public static void ClassInit(TestContext context)
