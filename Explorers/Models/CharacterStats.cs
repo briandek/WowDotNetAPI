022c5fdcd235060b2897817e01e02b3b9aaae2f8
Matt Dean <mjdean1994@gmail.com>
2015-12-21 16:10:32 -0500

    #75
    Updated Character Stats to work with current API. Should note that this
    will need re-updated with the launch of Legion's stat changes.

--- Explorers/Models/CharacterStats.cs
+++ Explorers/Models/CharacterStats.cs
@@ -14,31 +14,43 @@ namespace WowDotNetAPI.Models
         public int Health { get; set; }
 
         [DataMember(Name = "powerType")]
-        private string powerType { get; set; }
+        private string _powerType { get; set; }
 
         [DataMember(Name = "power")]
         public int Power { get; set; }
 
         [DataMember(Name = "str")]
-        public int Str { get; set; }
+        public int Strength { get; set; }
 
         [DataMember(Name = "agi")]
-        public int Agi { get; set; }
+        public int Agility { get; set; }
+
+        [DataMember(Name = "int")]
+        public int Intellect { get; set; }
 
         [DataMember(Name = "sta")]
-        public int Sta { get; set; }
-        
-        [DataMember(Name = "int")]
-        public int @Int { get; set; }
+        public int Stamina { get; set; }
 
-        [DataMember(Name = "spr")]
-        public int Spr { get; set; }
+        [DataMember(Name = "speedRating")]
+        public double SpeedRating { get; set; }
 
-        [DataMember(Name = "attackPower")]
-        public int AttackPower { get; set; }
+        [DataMember(Name = "speedRatingBonus")]
+        public double SpeedRatingBonus { get; set; }
 
-        [DataMember(Name = "rangedAttackPower")]
-        public int RangedAttackPower { get; set; }
+        [DataMember(Name = "crit")]
+        public double Crit { get; set; }
+
+        [DataMember(Name = "critRating")]
+        public double CritRating { get; set; }
+
+        [DataMember(Name = "haste")]
+        public double Haste { get; set; }
+
+        [DataMember(Name = "hasteRating")]
+        public double HasteRating { get; set; }
+
+        [DataMember(Name = "hasteRatingPercent")]
+        public double HasteRatingPercent { get; set; }
 
         [DataMember(Name = "mastery")]
         public double Mastery { get; set; }
@@ -46,29 +58,47 @@ namespace WowDotNetAPI.Models
         [DataMember(Name = "masteryRating")]
         public double MasteryRating { get; set; }
 
-        [DataMember(Name = "crit")]
-        public double Crit { get; set; }
+        [DataMember(Name = "spr")]
+        public int Spr { get; set; }
 
-        [DataMember(Name = "critRating")]
-		public double CritRating { get; set; }
+        [DataMember(Name = "bonusArmor")]
+        public int BonusArmor { get; set; }
 
-        [DataMember(Name = "hitPercent")]
-        public double HitPercent { get; set; }
+        [DataMember(Name = "multistrike")]
+        public double Multistrike { get; set; }
 
-        [DataMember(Name = "hitRating")]
-        public double HitRating { get; set; }
+        [DataMember(Name = "multistrikeRating")]
+        public double MultistrikeRating { get; set; }
 
-        [DataMember(Name = "haste")]
-        public double Haste { get; set; }
+        [DataMember(Name = "multistrikeRatingBonus")]
+        public double MultistrikeRatingBonus { get; set; }
 
-        [DataMember(Name = "hasteRating")]
-		public double HasteRating { get; set; }
+        [DataMember(Name = "leech")]
+        public double Leech { get; set; }
 
-        [DataMember(Name = "hasteRatingPercent")]
-        public double HasteRatingPercent { get; set; }
+        [DataMember(Name = "leechRating")]
+        public double LeechRating { get; set; }
 
-        [DataMember(Name = "expertiseRating")]
-		public double ExpertiseRating { get; set; }
+        [DataMember(Name = "leechRatingBonus")]
+        public double LeechRatingBonus { get; set; }
+
+        [DataMember(Name = "versatility")]
+        public int Versatility { get; set; }
+
+        [DataMember(Name = "versatilityDamageDoneBonus")]
+        public double VersatilityDamageDoneBonus { get; set; }
+
+        [DataMember(Name = "versatilityHealingDoneBonus")]
+        public double VersatilityHealingDoneBonus { get; set; }
+
+        [DataMember(Name = "versatilityDamageTakenBonus")]
+        public double VersatilityDamageTakenBonus { get; set; }
+
+        [DataMember(Name = "avoidanceRating")]
+        public double AvoidanceRating { get; set; }
+
+        [DataMember(Name = "avoidanceRatingBonus")]
+        public double AvoidanceRatingBonus { get; set; }
 
         [DataMember(Name = "spellPower")]
         public int SpellPower { get; set; }
@@ -80,22 +110,7 @@ namespace WowDotNetAPI.Models
         public double SpellCrit { get; set; }
 
         [DataMember(Name = "spellCritRating")]
-		public double SpellCritRating { get; set; }
-
-        [DataMember(Name = "spellHaste")]
-        public double SpellHaste { get; set; }
-
-        [DataMember(Name = "spellHasteRating")]
-        public double SpellHasteRating { get; set; }
-
-        [DataMember(Name = "spellHasteRatingPercent")]
-        public double SpellHasteRatingPercent { get; set; }
-
-        [DataMember(Name = "spellHitPercent")]
-        public double SpellHitPercent { get; set; }
-
-        [DataMember(Name = "spellHitRating")]
-		public double SpellHitRating { get; set; }
+        public double SpellCritRating { get; set; }
 
         [DataMember(Name = "mana5")]
         public double Mana5 { get; set; }
@@ -110,41 +125,20 @@ namespace WowDotNetAPI.Models
         public double Dodge { get; set; }
 
         [DataMember(Name = "dodgeRating")]
-		public double DodgeRating { get; set; }
+        public int DodgeRating { get; set; }
 
         [DataMember(Name = "parry")]
         public double Parry { get; set; }
 
         [DataMember(Name = "parryRating")]
-		public double ParryRating { get; set; }
+        public int ParryRating { get; set; }
 
         [DataMember(Name = "block")]
         public double Block { get; set; }
 
         [DataMember(Name = "blockRating")]
-        public double BlockRating { get; set; }
-
-        [DataMember(Name = "pvpResilience")]
-        public double PvpResilience { get; set; }
-
-        [DataMember(Name = "pvpResilienceBonus")]
-        public double PvPResilienceBonus { get; set; }
-
-        [DataMember(Name = "pvpResilienceRating")]
-        public int PvpResilienceRating { get; set; }
-
-        [DataMember(Name = "pvpPower")]
-        public double PvpPower { get; set; }
-
-        [DataMember(Name = "pvpPowerDamage")]
-        public double PvPPowerDamage { get; set; }
-
-        [DataMember(Name = "pvpPowerHealing")]
-        public Double PvPPowerHealing { get; set; }
-
-        [DataMember(Name = "pvpPowerRating")]
-        public int PvpPowerRating { get; set; }
-
+        public int BlockRating { get; set; }
+        
         [DataMember(Name = "mainHandDmgMin")]
         public double MainHandDmgMin { get; set; }
 
@@ -156,10 +150,7 @@ namespace WowDotNetAPI.Models
 
         [DataMember(Name = "mainHandDps")]
         public double MainHandDps { get; set; }
-
-        [DataMember(Name = "mainHandExpertise")]
-        public double MainHandExpertise { get; set; }
-
+        
         [DataMember(Name = "offHandDmgMin")]
         public double OffHandDmgMin { get; set; }
 
@@ -171,10 +162,7 @@ namespace WowDotNetAPI.Models
 
         [DataMember(Name = "offHandDps")]
         public double OffHandDps { get; set; }
-
-        [DataMember(Name = "offHandExpertise")]
-        public double OffHandExpertise { get; set; }
-
+        
         [DataMember(Name = "rangedDmgMin")]
         public double RangedDmgMin { get; set; }
 
@@ -186,34 +174,15 @@ namespace WowDotNetAPI.Models
 
         [DataMember(Name = "rangedDps")]
         public double RangedDps { get; set; }
+        
+        [DataMember(Name = "attackPower")]
+        public int AttackPower { get; set; }
 
-        [DataMember(Name = "rangedExpertise")]
-        public double RangedExpertise { get; set; }
-
-        [DataMember(Name = "rangedHaste")]
-        public double RangedHaste { get; set; }
-
-        [DataMember(Name = "rangedHasteRating")]
-        public double RangedHasteRating { get; set; }
-
-        [DataMember(Name = "rangedHasteRatingPercent")]
-        public double RangedHasteRatingPercent { get; set; }
-
-        [DataMember(Name = "rangedCrit")]
-        public double RangedCrit { get; set; }
-
-        [DataMember(Name = "rangedCritRating")]
-        public double RangedCritRating { get; set; }
-
-        [DataMember(Name = "rangedHitPercent")]
-        public double RangedHitPercent { get; set; }
-
-        [DataMember(Name = "rangedHitRating")]
-		public double RangedHitRating { get; set; }
+        [DataMember(Name = "rangedAttackPower")]
+        public int RangedAttackPower { get; set; }
 
 
-
-        public CharacterPowerType PowerType { get { return (CharacterPowerType)Enum.Parse(typeof(CharacterPowerType), powerType.Replace("-", string.Empty), true); } }
+        public CharacterPowerType PowerType { get { return (CharacterPowerType)Enum.Parse(typeof(CharacterPowerType), _powerType.Replace("-", string.Empty), true); } }
 
         //http://stackoverflow.com/questions/1447308/enumerating-through-an-objects-properties-string-in-c
         //TODO:REFACTOR THIS / possible performance issue
