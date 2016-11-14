bb86003931a37db129085c2afcd2a0e140871320
Tomas Blaho <xblaho@gmail.com>
2016-11-14 10:25:28 +0100

    added support for fetching character statistics

---
+++ Explorers/Models/CharacterStatistics.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace WowDotNetAPI.Models
+{
+    [DataContract]
+    public class CharacterStatistics
+    {
+        [DataMember(Name = "id")]
+        public int Id { get; set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "subCategories")]
+        public IEnumerable<CharacterStatisticsSubcategory> SubCategories { get; set; }
+    }
+}
